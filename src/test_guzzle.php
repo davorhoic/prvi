190f780256211b1dc80fc2e80cb55e64e4cec60f
mrvic <mrvic.predrag@gmail.com>
2023-12-14 19:05:22 +0100

    proba guzzle sa compezor rucno

---
+++ src/test_guzzle.php
@@ -0,0 +1,37 @@
+<?php
+require '../vendor/autoload.php';
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
+
+// Create a mock and queue two responses.
+$mock = new MockHandler([
+    new Response(200, ['X-Foo' => 'Bar'], 'Hello, World'),
+    new Response(202, ['Content-Length' => 0]),
+    new RequestException('Error Communicating with Server', new Request('GET', 'test'))
+]);
+
+$handlerStack = HandlerStack::create($mock);
+$client = new Client(['handler' => $handlerStack]);
+
+// The first request is intercepted with the first response.
+$response = $client->request('GET', '/');
+echo $response->getStatusCode();
+//> 200
+echo $response->getBody();
+//> Hello, World
+// The second request is intercepted with the second response.
+echo $client->request('GET', '/')->getStatusCode();
+//> 202
+
+// Reset the queue and queue up a new response
+$mock->reset();
+$mock->append(new Response(201));
+
+// As the mock was reset, the new response is the 201 CREATED,
+// instead of the previously queued RequestException
+echo $client->request('GET', '/')->getStatusCode();
+//> 201
